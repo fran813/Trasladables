c3c6a879c6dd8f04e298ed2b39452f988339cc1d
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2019-03-05 18:14:47 +0100

    Commit inicial con el esqueleto gradle de la aplicación.

---
+++ src/main/java/org/iesalandalus/programacion/trasladable/MainApp.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.trasladable;
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		System.out.println("Tarea presencial PROG05-08 2018/2019");
+	}
+	
+}
